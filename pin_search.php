ed2ed11919b52d4920551c8325550827479f37fa
ArchanRD <archanrd29@gmail.com>
2024-10-23 11:40:33 +0530

    Fix: Fixed navbar bugs

--- pin_search.php
+++ pin_search.php
@@ -8,99 +8,116 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] != true) {
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $pincode = $_POST['pincode'];
     $query = "SELECT * FROM `restaurant` WHERE `r_pincode` LIKE '$pincode' ORDER BY `r_rating` DESC";
-                $result = mysqli_query($conn, $query);
-                $num = mysqli_num_rows($result);
+    $result = mysqli_query($conn, $query);
+    $num = mysqli_num_rows($result);
 }
-
-
-?> 
-
-
+if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
+    $login_status = true;
+}
+?>
 
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js" integrity="sha384-Rx+T1VzGupg4BHQYs2gCW9It+akI2MM/mndMCy36UVfodzcJcF0GGLxZIzObiEfa" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js"
+        integrity="sha384-Rx+T1VzGupg4BHQYs2gCW9It+akI2MM/mndMCy36UVfodzcJcF0GGLxZIzObiEfa"
+        crossorigin="anonymous"></script>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="main.css">
+    <!-- <link rel="stylesheet" href="main.css"> -->
+    <link rel="stylesheet" href="output.css">
+    <!-- Google fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+    rel="stylesheet" />
+  <!-- Bootstrap icons  -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <title>Document</title>
 </head>
 
 <body>
 
-<div class="gtranslate_wrapper"></div>
-      <script>window.gtranslateSettings = {"default_language":"en","detect_browser_language":true,"wrapper_selector":".gtranslate_wrapper"}</script>
-      <script src="https://cdn.gtranslate.net/widgets/latest/float.js" defer></script>
+    <div class="gtranslate_wrapper"></div>
+    <script>window.gtranslateSettings = { "default_language": "en", "detect_browser_language": true, "wrapper_selector": ".gtranslate_wrapper" }</script>
+    <script src="https://cdn.gtranslate.net/widgets/latest/float.js" defer></script>
 
-    
-<nav class="nav">
-      <div class="nav__wrapper grid">
-          <div class="grid__span2 nav__logo-wrap">
-              <a href="index.php">
-                  <img class="nav__logo-img" src="images/logo/logo.webp">                
-              </a>
-          </div>
-          <div class="grid__span10 nav__links-wrap">
-              <ul class="nav__links">
 
-                  <li class="nav__link">
-                      
-                          <form action="pin_search.php" method="post">
-                          <input type="text" class="form-control " id="searchBar" name="pincode" placeholder="Search by Pincode" required>
-                  </li>
-                  <li class="nav__link">
-                      
-                          <input type="submit" class="btn-dark form-control " value="Search">
-                      </form>
-                  </li>
-                  <li class="nav__link">
-                      <a href="new_track_order.php">
-                          <img class="nav__link-icon" style="width: 1.8rem" src="images/favicons/tableware_50px.webp">
-                          <span class="nav__link-item">Orders</span> 
-                      </a>
-                  </li>
-                  <li class="nav__link">
-                      <a href="#contact">
-                          <img class="nav__link-icon" src="images/favicons/phone_32px.webp">
-                          <span class="nav__link-item">Contact </span>
-                      </a>
-                  </li>
-                  <li class="nav__link">
-                      <a href="user_logout.php">
-                          <img class="nav__link-icon" style="width: 1.9rem" src="images/favicons/user_male_circle_32px.webp">
-                          <span class="nav__link-item">Logout</span> 
-                      </a>
-                  </li>
-              </ul>
-          </div>
-          <div class="theme-switch-wrapper">
-            <label class="theme-switch" for="checkbox">
-              <input type="checkbox" id="checkbox" />
-              <div class="slider"></div>
-            </label>
-            <span id="mode-label">Light Mode</span>
-          </div>
-      </div>
-  </nav>
-<br>
-<br>
-<br>
-<br>
-  <div class="container mt-5">
-    <br>
-            <h1 class="card-title">Restaurants in "<?php echo $pincode ?>"</h1>
-            <br>
-            <div class="row">
-                <?php
-                
-                if ($num >= 1) {
-                    while ($row = mysqli_fetch_array($result)) {
-                        echo '<div class="col-md-4 mb-4">
+    <!-- navbar  -->
+    <nav
+        class="hidden lg:flex md:max-w-2xl lg:max-w-5xl xl:max-w-7xl w-full items-center justify-between max-w-7xl mx-auto font-poppins py-4">
+        <a href="index.php"><img src="./images/logo/logo.webp" alt="logo" class="w-36"></a>
+        <div class="flex sm:gap-1 md:gap-2 sm:text-sm md:text-base">
+            <a href="home.php"
+                class="hover:bg-gray-200 transition-all ease-in-out duration-100 active:bg-gray-300 focus:bg-gray-300 rounded-full hover:text-black py-2 px-4">Restaurants</a>
+            <a href="new_track_order.php"
+                class="hover:bg-gray-200 transition-all ease-in-out duration-100 active:bg-gray-300 focus:bg-gray-300 rounded-full hover:text-black py-2 px-4">Orders</a>
+            <a href="#"
+                class="hover:bg-gray-200 transition-all ease-in-out duration-100 active:bg-gray-300 focus:bg-gray-300 rounded-full hover:text-black py-2 px-4">Contact</a>
+            <?php if ($login_status == true) {
+                echo '<a href="profile.php" class="hover:bg-gray-200 transition-all ease-in-out duration-100 active:bg-gray-300 focus:bg-gray-300 rounded-full hover:text-black py-2 px-4">Account</a>';
+            } ?>
+        </div>
+        <div class="flex">
+
+            <div class="mx-3 sm:text-sm md:text-base">
+                <?php if ($login_status == true) {
+                    echo '<a href="user_logout.php" class="bg-red-500 hover:bg-red-600 transition-all ease-in-out duration-75 cursor-pointer w-max px-6 py-2 text-white rounded-full">Logout</a>';
+                } else {
+                    echo '<a href="user_login.php" class="bg-gray-900 hover:bg-gray-800 focus:border-white cursor-pointer w-max transition-all ease-in-out duration-75 px-6 py-2 text-white rounded-full">Login</a>';
+                } ?>
+            </div>
+    </nav>
+
+    <!-- nav for small device  -->
+    <div class="flex items-center justify-between max-w-7xl mx-auto font-poppins bg-white py-3 px-5 lg:hidden">
+        <a href="index.php"><img src="./images/logo/logo.webp" alt="logo" class="w-36 "></a>
+        <i class="bi bi-list menu select-none text-3xl"></i>
+    </div>
+    <div class="bg-gray-200 w-full top-5 font-poppins overflow-hidden px-5 py-3 hidden lg:hidden mb-5" id="nav-items">
+        <div class="flex flex-col gap-4">
+            <a href="home.php"
+                class="hover:bg-white focus:bg-white transition-all ease-in-out duration-100 py-2 px-3 rounded-md hover:text-black">Restaurants</a>
+            <a href="new_track_order.php"
+                class="hover:bg-white focus:bg-white transition-all ease-in-out duration-100 py-2 px-3 rounded-md hover:text-black">Orders</a>
+            <a href="#"
+                class="hover:bg-white focus:bg-white transition-all ease-in-out duration-100 py-2 px-3 rounded-md hover:text-black">Contact</a>
+            <?php if ($login_status == true) {
+                echo '<a href="profile.php" class="hover:bg-white focus:bg-white transition-all ease-in-out duration-100 py-2 px-3 rounded-md hover:text-black">Account</a>';
+            } ?>
+            <div>
+                <h2 class="text-base text-gray-400 mt-3">User actions</h2>
+                <div class="h-[1px] bg-gray-300 w-full"></div>
+            </div>
+            <?php if ($login_status == true) {
+                echo '<a href="user_logout.php" class="bg-red-500 hover:bg-red-600 transition-all ease-in-out duration-75 cursor-pointer w-max px-6 py-2 text-white rounded-full">Logout</a>';
+            } else {
+                echo '<a href="user_login.php" class="bg-gray-900 hover:bg-gray-800 focus:border-white cursor-pointer w-max transition-all ease-in-out duration-75 px-6 py-2 text-white rounded-full">Login</a>';
+            }
+            ?>
+        </div>
+    </div>
+
+    <div class="container mt-5">
+        <br>
+        <h1 class="card-title">Restaurants in "<?php echo $pincode ?>"</h1>
+        <br>
+        <div class="row">
+            <?php
+
+            if ($num >= 1) {
+                while ($row = mysqli_fetch_array($result)) {
+                    echo '<div class="col-md-4 mb-4">
                                   <div class="card">
                                     <img src="vendor/' . $row['r_bg'] . '" class="card-img-top" alt="' . $row['r_name'] . '">
                                     <div class="card-body">
@@ -111,17 +128,18 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
                                     </div>
                                   </div>
                               </div>';
-                    }
-                } else {
-                    echo '<div class="col">
+                }
+            } else {
+                echo '<div class="col">
                               <p>No restaurants found.</p>
                           </div>';
-                }
-                ?>
-            </div>
+            }
+            ?>
+        </div>
 
-  </div>
-  <script src="./darkMode.js"></script>
+    </div>
+    <script src="menu.js"></script>
+    <script src="./darkMode.js"></script>
 </body>
 
 </html>
