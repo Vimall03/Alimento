70bd86a7f378b90f05bee489f73e5c0eb1285ac4
Shivansh <shivanshsaxena2910@gmail.com>
2024-11-06 22:34:20 +0530

    global context and seperation of customer and vendor side of website

---
+++ alimento-nextjs/app/vendor/layout.tsx
@@ -0,0 +1,14 @@
+
+export default function VendorLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+          {children}
+      </body>
+    </html>
+  );
+}
---
+++ alimento-nextjs/context/dishFormContext.tsx
@@ -0,0 +1,119 @@
+"use client";
+import { Category, Tag } from "@prisma/client";
+import {
+  createContext,
+  ReactNode,
+  useCallback,
+  useContext,
+  useState,
+} from "react";
+import toast from "react-hot-toast";
+
+interface GlobalContextType {
+  currentStep: number;
+  setCurrentStep: (step: number) => void;
+  completed: boolean;
+  setCompleted: (completed: boolean) => void;
+
+  dishName: string;
+  setDishName: (name: string) => void;
+  dishPrice: number;
+  setDishPrice: (price: number) => void;
+  dishDescription: string;
+  setDishDescription: (description: string) => void;
+  dishCategory: Category;
+  setDishCategory: (category: Category) => void;
+  dishTags: Tag[];
+  setDishTags: (tags: Tag[]) => void;
+
+  validDishName: boolean;
+  setValidDishName: (valid: boolean) => void;
+  validDishPrice: boolean;
+  setValidDishPrice: (valid: boolean) => void;
+  validDishDescription: boolean;
+  setValidDishDescription: (valid: boolean) => void;
+  validDishCategory: boolean;
+  setValidDishCategory: (valid: boolean) => void;
+  validDishTags: boolean;
+  setValidDishTags: (valid: boolean) => void;
+
+  checkedBox: boolean;
+  setCheckedBox: (checkedBox: boolean) => void;
+
+  formCompleted: boolean;
+  setFormCompleted: (completed: boolean) => void;
+}
+
+export const GlobalDishContext = createContext<GlobalContextType | undefined>(
+  undefined
+);
+
+export const useGlobalDish = () => {
+  const context = useContext(GlobalDishContext);
+  if (!context) {
+    throw new Error("useGlobalDish must be used within a GlobalDishProvider");
+  }
+  return context;
+};
+
+export const GlobalDishProvider = ({ children }: { children: ReactNode }) => {
+  const [currentStep, setCurrentStep] = useState(1);
+  const [completed, setCompleted] = useState(false);
+
+  const [dishName, setDishName] = useState("");
+  const [dishPrice, setDishPrice] = useState(0);
+  const [dishDescription, setDishDescription] = useState("");
+  const [dishCategory, setDishCategory] = useState<Category>(
+    Category.APPETIZER
+  );
+  const [dishTags, setDishTags] = useState<Tag[]>([]);
+
+  const [validDishName, setValidDishName] = useState(false);
+  const [validDishPrice, setValidDishPrice] = useState(false);
+  const [validDishDescription, setValidDishDescription] = useState(false);
+  const [validDishCategory, setValidDishCategory] = useState(false);
+  const [validDishTags, setValidDishTags] = useState(false);
+
+  const [checkedBox, setCheckedBox] = useState(false);
+  const [formCompleted, setFormCompleted] = useState(false);
+
+  return (
+    <GlobalDishContext.Provider
+      value={{
+        currentStep,
+        setCurrentStep,
+        completed,
+        setCompleted,
+
+        dishName,
+        setDishName,
+        dishPrice,
+        setDishPrice,
+        dishDescription,
+        setDishDescription,
+        dishCategory,
+        setDishCategory,
+        dishTags,
+        setDishTags,
+
+        validDishName,
+        setValidDishName,
+        validDishPrice,
+        setValidDishPrice,
+        validDishDescription,
+        setValidDishDescription,
+        validDishCategory,
+        setValidDishCategory,
+        validDishTags,
+        setValidDishTags,
+
+        checkedBox,
+        setCheckedBox,
+        formCompleted,
+        setFormCompleted,
+      }}
+    >
+      {children}
+    </GlobalDishContext.Provider>
+  );
+};
